345b8f2354442d41ce4db27c912f03a8721dfc4f
epsilon91103 <Ijnmko059>
2021-02-09 20:56:42 +0500

    Change setUp method

--- test.py
+++ test.py
@@ -6,57 +6,65 @@ from ducks import Duck
 
 
 class TestAnimals:
+    validation = {}
+
     def test_init(self):
         self.animal.say()
-        self.assertEqual(self.animal.get_energy(), self.results['init'])
+        self.assertEqual(self.animal.get_energy(), self.validation['init'])
 
     def test_run(self):
         self.animal.run()
-        self.assertEqual(self.animal.get_energy(), self.results['run'])
+        self.assertEqual(self.animal.get_energy(), self.validation['run'])
 
     def test_swim(self):
         self.animal.swim()
-        self.assertEqual(self.animal.get_energy(), self.results['swim'])
+        self.assertEqual(self.animal.get_energy(), self.validation['swim'])
 
     def test_fly(self):
         self.animal.fly()
-        self.assertEqual(self.animal.get_energy(), self.results['fly'])
+        self.assertEqual(self.animal.get_energy(), self.validation['fly'])
 
 
 class TestDog(TestAnimals, unittest.TestCase):
+    validation = {'init': 100, 'run': 90, 'swim': 70, 'fly': 100}
+
     def setUp(self):
         self.animal = Dog("Bobik")
-        self.results = {'init': 100, 'run': 90, 'swim': 70, 'fly': 100}
 
 
 class TestCat(TestAnimals, unittest.TestCase):
+    validation = {'init': 100, 'run': 95, 'swim': 100, 'fly': 100}
+
     def setUp(self):
         self.animal = Cat("Kitty")
-        self.results = {'init': 100, 'run': 95, 'swim': 100, 'fly': 100}
 
 
 class TestTiger(TestAnimals, unittest.TestCase):
+    validation = {'init': 200, 'run': 180, 'swim': 160, 'fly': 200}
+
     def setUp(self):
         self.animal = Tiger("Kitty")
-        self.results = {'init': 200, 'run': 180, 'swim': 160, 'fly': 200}
 
 
 class TestFishSimple(TestAnimals, unittest.TestCase):
+    validation = {'init': 100, 'run': 100, 'swim': 95, 'fly': 100}
+
     def setUp(self):
         self.animal = FishSimple("Snappy")
-        self.results = {'init': 100, 'run': 100, 'swim': 95, 'fly': 100}
 
 
 class TestFishCanFly(TestAnimals, unittest.TestCase):
+    validation = {'init': 100, 'run': 100, 'swim': 95, 'fly': 80}
+
     def setUp(self):
         self.animal = FishCanFly("MightySnappy")
-        self.results = {'init': 100, 'run': 100, 'swim': 95, 'fly': 80}
 
 
 class TestDuck(TestAnimals, unittest.TestCase):
+    validation = {'init': 100, 'run': 100, 'swim': 90, 'fly': 70}
+
     def setUp(self):
         self.animal = Duck("Govard")
-        self.results = {'init': 100, 'run': 100, 'swim': 90, 'fly': 70}
 
 
 if __name__ == '__main__':
