6ab3ab71dfbbe01d07c104eb309b0b398d690819
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2021-02-11 17:22:40 +0500

    Update ducks.py

--- ducks.py
+++ ducks.py
@@ -1,14 +1,6 @@
-from basic_classes import Animal
+from basic_classes import FlyAnimal, SwimAnimal
 
 
-class Duck(Animal):
-    def __init__(self, name, energy=100, init_parameters=None):
-        super().__init__(name, energy, init_parameters)
-
-    def swim(self):
-        print(f"My name is '{self.name}' and i swimming")
-        self.energy = self.energy - 10
-
-    def fly(self):
-        print(f"My name is '{self.name}' and i flying")
-        self.energy = self.energy - 30
+class Duck(SwimAnimal, FlyAnimal):
+    def __init__(self, name, energy=100, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
