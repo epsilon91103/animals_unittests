8f61de18b85d4bfe118ba8975f0e37efa7f66e40
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2021-02-11 17:23:39 +0500

    Update test.py

--- test.py
+++ test.py
@@ -29,42 +29,42 @@ class TestDog(TestAnimals, unittest.TestCase):
     validation = {'init': 100, 'run': 90, 'swim': 70, 'fly': 100}
 
     def setUp(self):
-        self.animal = Dog("Bobik")
+        self.animal = Dog("Bobik", run_energy=10, swim_energy=30)
 
 
 class TestCat(TestAnimals, unittest.TestCase):
     validation = {'init': 100, 'run': 95, 'swim': 100, 'fly': 100}
 
     def setUp(self):
-        self.animal = Cat("Kitty")
+        self.animal = Cat("Kitty", run_energy=5)
 
 
 class TestTiger(TestAnimals, unittest.TestCase):
     validation = {'init': 200, 'run': 180, 'swim': 160, 'fly': 200}
 
     def setUp(self):
-        self.animal = Tiger("Kitty")
+        self.animal = Tiger("Kitty", swim_energy=40, run_energy=20)
 
 
 class TestFishSimple(TestAnimals, unittest.TestCase):
     validation = {'init': 100, 'run': 100, 'swim': 95, 'fly': 100}
 
     def setUp(self):
-        self.animal = FishSimple("Snappy")
+        self.animal = FishSimple("Snappy", swim_energy=5)
 
 
 class TestFishCanFly(TestAnimals, unittest.TestCase):
     validation = {'init': 100, 'run': 100, 'swim': 95, 'fly': 80}
 
     def setUp(self):
-        self.animal = FishCanFly("MightySnappy")
+        self.animal = FishCanFly("MightySnappy", swim_energy=5, fly_energy=20)
 
 
 class TestDuck(TestAnimals, unittest.TestCase):
     validation = {'init': 100, 'run': 100, 'swim': 90, 'fly': 70}
 
-    def setUp(self):
-        self.animal = Duck("Govard")
+    def setUp(self, a=5):
+        self.animal = Duck("Govard", swim_energy=10, fly_energy=30)
 
 
 if __name__ == '__main__':
