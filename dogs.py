57a5059309d924db8959f30c041d8b710ed1fb66
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2021-02-11 17:22:16 +0500

    Update dogs.py

--- dogs.py
+++ dogs.py
@@ -1,14 +1,6 @@
-from basic_classes import Animal
+from basic_classes import RunAnimal, SwimAnimal
 
 
-class Dog(Animal):
-    def __init__(self, name, energy=100, init_parameters=None):
-        super().__init__(name, energy, init_parameters)
-
-    def run(self):
-        print(f"My name is '{self.name}' and i running")
-        self.energy = self.energy - 10
-
-    def swim(self):
-        print(f"My name is '{self.name}' and i swimming")
-        self.energy = self.energy - 30
+class Dog(RunAnimal, SwimAnimal):
+    def __init__(self, name, energy=100, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
