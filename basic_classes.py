ee172a472f505bee9ac4d4c45cbedbc479bb30ff
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2021-02-11 17:21:26 +0500

    Update basic_classes.py

--- basic_classes.py
+++ basic_classes.py
@@ -1,19 +1,7 @@
 class Animal:
-    def __init__(self, name, energy, init_parameters=None):
-        if init_parameters is None:
-            init_parameters = {}
-
-        if init_parameters:
-            self.name = init_parameters['name']
-            if 'energy' in init_parameters.keys():
-                condition = init_parameters["energy"]
-            else:
-                condition = energy
-        else:
-            self.name = name
-            condition = energy
-
-        self.energy = condition
+    def __init__(self, name, energy, **init_parameters):
+        self.name = init_parameters.get('name', name)
+        self.energy = init_parameters.get('energy', energy)
 
     def say(self):
         print(f"Hello, i'm {type(self).__name__} and my name is {self.name}")
@@ -29,3 +17,33 @@ class Animal:
 
     def get_energy(self):
         return self.energy
+
+
+class RunAnimal(Animal):
+    def __init__(self, name, energy, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
+        self.run_energy = init_parameters['run_energy']
+
+    def run(self):
+        print(f"My name is '{self.name}' and i running")
+        self.energy -= self.run_energy
+
+
+class SwimAnimal(Animal):
+    def __init__(self, name, energy, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
+        self.swim_energy = init_parameters['swim_energy']
+
+    def swim(self):
+        print(f"My name is '{self.name}' and i swimming")
+        self.energy -= self.swim_energy
+
+
+class FlyAnimal(Animal):
+    def __init__(self, name, energy, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
+        self.fly_energy = init_parameters['fly_energy']
+
+    def fly(self):
+        print(f"My name is '{self.name}' and i flying")
+        self.energy -= self.fly_energy
