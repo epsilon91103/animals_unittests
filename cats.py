c752017f2e44c702974f5a58dd93cb2bb5a536af
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2021-02-11 17:21:58 +0500

    Update cats.py

--- cats.py
+++ cats.py
@@ -1,23 +1,11 @@
-from basic_classes import Animal
+from basic_classes import RunAnimal, SwimAnimal
 
 
-class Cat(Animal):
-    def __init__(self, name, energy=100, init_parameters=None):
-        super().__init__(name, energy, init_parameters)
-
-    def run(self):
-        print(f"My name is '{self.name}' and i running")
-        self.energy = self.energy - 5
+class Cat(RunAnimal):
+    def __init__(self, name, energy=100, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
 
 
-class Tiger(Animal):
-    def __init__(self, name, energy=200, init_parameters=None):
-        super().__init__(name, energy, init_parameters)
-
-    def run(self):
-        print(f"My name is '{self.name}' and i running")
-        self.energy = self.energy - 20
-
-    def swim(self):
-        print(f"My name is '{self.name}' and i swimming")
-        self.energy = self.energy - 40
+class Tiger(Cat, SwimAnimal):
+    def __init__(self, name, energy=200, **init_parameters):
+        super().__init__(name, energy, **init_parameters)
